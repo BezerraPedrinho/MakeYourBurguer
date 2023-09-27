f5c674386c6176d19d898a0c6e9eed398ac1fe0e
BezerraPedrinho <phdesign9@gmail.com>
2023-09-27 08:37:17 -0300

    style nav and footer

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <NavBar />
+  <NavBar :logo="logo_src" :alt="app_name" />
   <router-view />
   <Footer />
 </template>
@@ -11,6 +11,29 @@ export default {
     NavBar,
     Footer,
   },
+  data() {
+    return {
+      logo_src: "/img/logo.png",
+      app_name: "Make U Burger",
+    };
+  },
 };
 </script>
-<style></style>
+<style>
+* {
+  font-family: Helvetica, sans-serif;
+  padding: 0px;
+  margin: 0px;
+  box-sizing: border-box;
+}
+.main-container {
+  margin: 50px;
+  min-height: 250px;
+}
+h1 {
+  text-align: center;
+  font-size: 42px;
+  margin-bottom: 30px;
+  color: #222;
+}
+</style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,24 @@
-<template></template>
+<template>
+  <footer class="fotter-container">
+    <p>Make u Burger &copy; 2023</p>
+  </footer>
+</template>
 <script>
 export default {
   name: "Footer",
 };
 </script>
-<style scoped></style>
+<style scoped>
+.fotter-container {
+  height: 50px;
+  background-color: #222;
+  border-top: 4px solid #111;
+  color: #fcba03;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  position: absolute;
+  bottom: 0px;
+}
+</style>
